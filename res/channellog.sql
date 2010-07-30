d82173b517878cde46d1c2eb08af4e365f33b03b
Paul <pvince@gmail.com>
2010-07-30 14:34:40 +0000

    Made the MySQL queries safer, added the database schema I am using.

---
+++ res/channellog.sql
@@ -0,0 +1,54 @@
+-- phpMyAdmin SQL Dump
+-- version 3.2.0.1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Jul 30, 2010 at 02:31 PM
+-- Server version: 5.1.36
+-- PHP Version: 5.3.0
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `channellog`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `channel`
+--
+
+CREATE TABLE IF NOT EXISTS `channel` (
+  `pk_ChannelID` int(11) NOT NULL AUTO_INCREMENT,
+  `vcServer` varchar(256) CHARACTER SET latin1 NOT NULL,
+  `vcChannel` varchar(512) CHARACTER SET latin1 NOT NULL,
+  PRIMARY KEY (`pk_ChannelID`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=2 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `messages`
+--
+
+CREATE TABLE IF NOT EXISTS `messages` (
+  `pk_MessageID` bigint(20) NOT NULL AUTO_INCREMENT,
+  `fk_ChannelID` int(11) NOT NULL,
+  `tsMsgTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `intMsgType` int(11) NOT NULL,
+  `vcUsername` varchar(255) NOT NULL,
+  `vcMessage` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`pk_MessageID`),
+  KEY `vcUsername` (`vcUsername`),
+  KEY `fk_ChannelID` (`fk_ChannelID`),
+  FULLTEXT KEY `vcMessage` (`vcMessage`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=17 ;
+
+--
--- src/jircbot/jIRCTools.java
+++ src/jircbot/jIRCTools.java
@@ -4,10 +4,18 @@ import static com.rosaloves.bitlyj.Bitly.*;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+/*
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+*/
 public class jIRCTools {
 	public static String bitlyName = "";
 	public static String bitlyAPIKey = "";
@@ -23,39 +31,19 @@ public class jIRCTools {
 	}
 	
 	public static void insertMessage(String channel, String server, String username, String msg) {
-	    String query = "INSERT INTO messages ";
-	    query += "( fk_ChannelID, intMsgType, vcUsername, vcMessage )";
-	    query += "VALUES ";
-	    query += "( 1, 1, \"" + username + "\", \"" + msg + "\" )";
-        
-	    updateDatabase(query);
-	}
-	
-	private static ResultSet queryDatabase(String statement) {
-	    ResultSet rs = null;
-	    
+	    String insertStatement = "INSERT INTO messages " +
+	    		"( fk_ChannelID, intMsgType, vcUsername, vcMessage )" +
+	    		"VALUES" +
+	    		"( ?, ?, ?, ? )";
 	    try {
             Class.forName("com.mysql.jdbc.Driver");
             Connection conn = DriverManager.getConnection("jdbc:mysql://192.168.2.143:3306/channellog", "chanlogger", "qF7yWI4Tcnsb4qD");
-            Statement stmt = conn.createStatement();
-            
-            rs = stmt.executeQuery(statement);
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-	    
-	    return rs;
-	}
-	
-	private static void updateDatabase(String statement) {
-	    try {
-            Class.forName("com.mysql.jdbc.Driver");
-            Connection conn = DriverManager.getConnection("jdbc:mysql://192.168.2.143:3306/channellog", "chanlogger", "qF7yWI4Tcnsb4qD");
-            Statement stmt = conn.createStatement();
-            
-            stmt.execute(statement);
+            PreparedStatement stmt = conn.prepareStatement(insertStatement);
+            stmt.setInt(1, 1);
+            stmt.setInt(2, 1);
+            stmt.setString(3, username);
+            stmt.setString(4, msg);
+            stmt.executeUpdate();
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         } catch (SQLException e) {
