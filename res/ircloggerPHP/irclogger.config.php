16897fb7a450e0532008b28691b8514c7b385535
Paul <pvince@gmail.com>
2010-08-05 13:23:56 +0000

    Improved the PHP to not use a hard coded IRC channel & Server.  Removed an accidently added php file.

--- res/ircloggerPHP/irclogger.config.php
+++ res/ircloggerPHP/irclogger.config.php
@@ -4,9 +4,12 @@
 
 # DB access used by frontend and logger
 db_host = localhost
-db_user = chanlogger
-db_pass = qF7yWI4Tcnsb4qD
+db_user = paulbot
+db_pass = paulb0t
 db_name = channellog
+
+irc_channel = #hive13
+irc_server = irc.freenode.net
 
 # where is the frontend installed to?
 baseurl = http://caffeinesystems.ath.cx/irclogger/
