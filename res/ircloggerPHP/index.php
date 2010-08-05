e0696f837b43defa315acdae1069f7d509c257fd
Paul <pvince@gmail.com>
2010-08-05 02:54:23 +0000

    Adding the php website.

---
+++ res/ircloggerPHP/index.php
@@ -0,0 +1,148 @@
+<?php
+
+require("func.php");
+$conf = loadconfig('irclogger.config.php');
+
+
+header('Content-Type: text/html; charset=utf-8');
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+ "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en" dir="ltr">
+<head>
+    <title>IRC channel log</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link rel="stylesheet" media="all" type="text/css" href="style.css" />
+</head>
+<body>
+<?php
+$dbh = getdbhandle($conf);
+
+if(isSet($_REQUEST['s'])){
+    $sql = "SELECT pk_MessageID, DATE(tsMsgTime) as d, TIME(tsMsgTime) as t, vcUsername, vcMsgType, vcMessage
+              FROM messages, channel 
+             WHERE fk_ChannelID = pk_ChannelID AND vcChannel = '#hive13_test' AND 
+             MATCH (vcMessage)
+           AGAINST ('".addslashes($_REQUEST['s'])."')
+          ORDER BY tsMsgTime DESC, pk_MessageID DESC";
+}elseif(isSet($_REQUEST['u'])){
+    $sql = "SELECT tsMsgTime
+              FROM messages, channel
+             WHERE fk_ChannelID = pk_ChannelID AND vcChannel = '#hive13_test' AND
+                   vcUsername = '".addslashes($_REQUEST['u'])."'
+          ORDER BY tsMsgTime DESC, pk_MessageID DESC
+             LIMIT 1";
+    $res = mysql_query($sql,$dbh);
+    $row = mysql_fetch_array($res, MYSQL_ASSOC);
+    if($row['tsMsgTime']){
+        $sql = "SELECT pk_MessageID, DATE(tsMsgTime) as d, TIME(tsMsgTime) as t, vcUsername, vcMsgType, vcMessage
+              FROM messages, channel
+             WHERE fk_ChannelID = pk_ChannelID AND vcChannel = '#hive13_test' AND
+                   tsMsgTime >= '".addslashes($row['tsMsgTime'])."'
+          ORDER BY tsMsgTime, pk_MessageID";
+    }else{
+        echo 'Could not find any logs for user '.htmlspecialchars($_REQUEST['u']);
+        $sql = '';
+    }
+}else{
+    if(isSet($_REQUEST['d'])){
+        $date = $_REQUEST['d'];
+    }else{
+        $date = date('Y-m-d');
+    }
+
+    $sql = "SELECT pk_MessageID, DATE(tsMsgTime) as d, TIME(tsMsgTime) as t, vcUsername, vcMsgType, vcMessage
+              FROM messages, channel
+             WHERE fk_ChannelID = pk_ChannelID AND vcChannel = '#hive13_test' AND
+                   DATE(tsMsgTime) = DATE('".addslashes($date)."')
+          ORDER BY tsMsgTime, pk_ChannelID";
+}
+
+if($sql) $res = mysql_query($sql,$dbh);
+
+if($date){
+    echo '<form action="index.php">';
+    echo '<h1>Log for <input type="text" name="d" value="'.htmlspecialchars($date).'" /></h1>';
+    echo '</form>';
+}elseif($sql && $_REQUEST['u']){
+    echo '<h1>Log since last login of '.htmlspecialchars($_REQUEST['u']).'</h1>';
+}elseif($sql && $_REQUEST['s']){
+    echo '<h1>Matching lines for '.htmlspecialchars($_REQUEST['s']).'</h1>';
+    echo '<p>Click the timestamp to see the line in context.</p>';
+}
+
+echo '<ul id="log">';
+if($sql) while($row = mysql_fetch_array($res, MYSQL_ASSOC)){
+    echo '<li>';
+    echo '<a id="msg'.$row['pk_MessageID'].'" href="index.php?d='.$row['d'].'#msg'.$row['pk_MessageID'].'" class="time">';
+    echo '['.$row['t'].']';
+    echo '</a>';
+    if($row['vcMsgType'] == 'publicMsg'){
+        echo '<b style="color:#'.substr(md5($row['vcUsername']),0,6).'">'.htmlspecialchars($row['vcUsername']).'</b><span class="user">';
+    }else{
+        echo '<b>*</b><span class="server">';
+    }
+    $msg = htmlspecialchars(  $row['vcMessage']);
+    $msg = preg_replace_callback('/((https?|ftp):\/\/[\w-?&;#~=\.\/\@%:]+[\w\/])/ui',
+                                 'format_link',$msg);
+
+    if(substr($msg,0,3) == '/me'){
+        $msg = '<strong>'.htmlspecialchars($row['vcUsername']).substr($msg,3).'</strong>';
+    }
+    echo $msg;
+    echo '</span>';
+    echo '</li>';
+
+}
+echo '</ul>';
+
+$sql = "SELECT DISTINCT DATE(tsMsgTime) as d, DAY(tsMsgTime) as day
+          FROM messages, channel
+         WHERE fk_ChannelID = pk_ChannelID AND vcChannel = '#hive13_test' AND
+               tsMsgTime > DATE_SUB(NOW(), INTERVAL 30 DAY)
+      ORDER BY tsMsgTime";
+$res = mysql_query($sql,$dbh);
+
+echo '<div class="archive">Last 30 days: ';
+while($row = mysql_fetch_array($res, MYSQL_ASSOC)){
+    echo '<a href="index.php?d='.$row['d'].'">'.$row['day'].'</a> ';
+}
+echo '</div>';
+
+?>
+
+<div class="footer"><div>
+<ul>
+    <li><a href="index.php?d=<?php echo date('Y-m-d')?>">Today's log</a></li>
+    <li><a href="index.php?d=<?php echo date('Y-m-d',time()-(60*60*24))?>">Yesterday's log</a></li>
+</ul>
+<ul>
+    <li>What happened since your last login?<br />
+        <form action="index.php"><input name="u" /></form>
+        <small>(Give your nick name and hit enter)</small></li>
+    <li>Search:<br />
+        <form action="index.php"><input name="s" /></form>
+        <small>(Give search terms and hit enter)</small></li>
+</ul>
+<ul>
+    <li style="width: 25em;">Powered by a homemade, experimental IRC logger written in Perl, PHP and MySQL.<br />
+    A <a href="http://www.splitbrain.org">splitbrain.org</a> service.</li>
+</ul>
+</div></div>
+
+</body>
+</html>
+<?php
+
+/**
+ * Callback to autolink a URL (with shortening)
+ */
+function format_link($match){
+    $url = $match[1];
+    $url = str_replace("\\\\'","'",$url);
+    $link = '<a href="'.$url.'" rel="nofollow">'.$url.'</a>';
+    return $link;
+}
+
+
+?>
