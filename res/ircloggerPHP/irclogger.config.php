2dda2473886a1929f9caa0801c60920909c6e808
Paul <pvince@gmail.com>
2010-08-05 02:54:23 +0000

    Adding the php website.

---
+++ res/ircloggerPHP/irclogger.config.php
@@ -0,0 +1,12 @@
+# configuration file for irc logger
+#
+#
+
+# DB access used by frontend and logger
+db_host = localhost
+db_user = chanlogger
+db_pass = qF7yWI4Tcnsb4qD
+db_name = channellog
+
+# where is the frontend installed to?
+baseurl = http://caffeinesystems.ath.cx/irclogger/
