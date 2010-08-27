bfb027a25962cecfbdb5622f5a095bb0f15614bf
Paul <pvince@gmail.com>
2010-08-27 19:39:50 +0000

    Added support for multiple tries at getting the short URL title.

--- src/jircbot/commands/jIBCLinkify.java
+++ src/jircbot/commands/jIBCLinkify.java
@@ -8,6 +8,11 @@ import jircbot.support.jIRCTools;
 public class jIBCLinkify extends jIBCommand {
 
 	public final int MAX_URL_LENGTH = 25;
+	
+	public final boolean WAIT_FOR_TITLE = true;
+	
+	public final int WAIT_FOR_TITLE_TIMEOUT = 5000;
+	
     @Override
     public String getCommandName() {
         return "Linkify";
@@ -25,7 +30,22 @@ public class jIBCLinkify extends jIBCommand {
         	String url = "";
         	if((url = m.group()).length() > MAX_URL_LENGTH) {
 	            String shortURL = jIRCTools.generateShortURL(url);
-	            String urlTitle = jIRCTools.getShortURLTitle(shortURL);
+	            String urlTitle;
+	            int sleepCount = 0;
+	            // The title is not retrieved by bit.ly immediately, we can optionally
+	            // move on, likely without the title, or we can repeatedly try
+	            // until we get a response or timeout.
+	            while((urlTitle = jIRCTools.getShortURLTitle(shortURL)).isEmpty()
+	            		&& WAIT_FOR_TITLE && (sleepCount*200) < WAIT_FOR_TITLE_TIMEOUT) {
+	            	try {
+						Thread.sleep(200);
+						if(sleepCount % 5 == 0)
+							bot.log("Waiting for URL title [ " + sleepCount*200 + " ms ]");
+						sleepCount += 1;
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+	            }
 	            returnMsg += urlTitle + " [ " + shortURL + " ]; ";
         	}
         }
