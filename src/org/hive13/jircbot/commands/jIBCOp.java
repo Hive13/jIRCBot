436e2ba60616ec45e5bfafd91fd9933e5f33152b
Paul <pvince@gmail.com>
2010-10-13 03:53:39 +0000

    New Command

---
+++ src/org/hive13/jircbot/commands/jIBCOp.java
@@ -0,0 +1,24 @@
+package org.hive13.jircbot.commands;
+
+import org.hive13.jircbot.jIRCBot;
+import org.hive13.jircbot.support.jIRCUser.eAuthLevels;
+
+public class jIBCOp extends jIBCommand {
+
+    public jIBCOp() {
+        super(eAuthLevels.operator);
+    }
+    
+    @Override
+    public String getCommandName() {
+        return "Op";
+    }
+
+    @Override
+    protected void handleMessage(jIRCBot bot, String channel, String sender,
+            String message) {
+        //String splitMsg
+        bot.op(channel, message);
+    }
+
+}
