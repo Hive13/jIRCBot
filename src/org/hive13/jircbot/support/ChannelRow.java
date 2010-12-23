8ee53fe1bea3e9a1072730367bec4a1ddee02c7d
Paul Vincent <pvince@gmail.com>
2010-12-23 13:55:19 -0500

    Rounded out this class to match MessageRow in terms of functionality.

--- src/org/hive13/jircbot/support/ChannelRow.java
+++ src/org/hive13/jircbot/support/ChannelRow.java
@@ -1,5 +1,8 @@
 package org.hive13.jircbot.support;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class ChannelRow {
 	public int pk_ChannelID;
 	public String vcServer, vcChannel;
@@ -7,6 +10,18 @@ public class ChannelRow {
 		this.pk_ChannelID = pk_ChannelID;
 		this.vcServer = vcServer;
 		this.vcChannel = vcChannel;
+		
 	}
 	public ChannelRow() {}
+	public ChannelRow(ResultSet rs) throws SQLException {
+		this(rs, true);
+	}
+	public ChannelRow(ResultSet rs, boolean validateNames) throws SQLException {
+		if(validateNames || jIRCTools.isValidColumn(rs, "pk_ChannelID"))
+            this.pk_ChannelID = rs.getInt("pk_ChannelID");
+		if(validateNames || jIRCTools.isValidColumn(rs, "vcServer"))
+            this.vcServer = rs.getString("vcServer");
+		if(validateNames || jIRCTools.isValidColumn(rs, "vcChannel"))
+            this.vcChannel = rs.getString("vcChannel");
+	}
 }
