f7830ebdf0e21d8cf3c3ef46a0840b4905c6ae71
Paul Vincent <pvince@gmail.com>
2010-12-23 14:34:43 -0500

    Fixed the initialization of ChannelRow member variables.

--- src/org/hive13/jircbot/support/ChannelRow.java
+++ src/org/hive13/jircbot/support/ChannelRow.java
@@ -4,8 +4,9 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class ChannelRow {
-	public int pk_ChannelID;
-	public String vcServer, vcChannel;
+	public int pk_ChannelID = -1;
+	public String vcServer = null, vcChannel = null;
+	
 	public ChannelRow(int pk_ChannelID, String vcServer, String vcChannel) {
 		this.pk_ChannelID = pk_ChannelID;
 		this.vcServer = vcServer;
