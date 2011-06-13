5917ca1371bee8382d44c607ab4dd5e69684eed5
zombieCraig <agent.craig@gmail.com>
2011-06-13 14:52:57 -0400

    Added ability to show bitcoin ticker data every ten minutes during an active bitcoin conversation

--- src/org/hive13/jircbot/commands/jIBCBitcoin.java
+++ src/org/hive13/jircbot/commands/jIBCBitcoin.java
@@ -1,5 +1,7 @@
 package org.hive13.jircbot.commands;
 
+import java.util.Calendar;
+
 import org.hive13.jircbot.jIRCBot;
 import org.hive13.jircbot.support.MtGoxTicker;
 import org.hive13.jircbot.support.jIRCTools;
@@ -7,7 +9,8 @@ import org.hive13.jircbot.support.jIRCTools;
 import com.google.gson.Gson;
 
 public class jIBCBitcoin extends jIBCommand {
-	private final String MT_GOX_URL = "https://mtgox.com/code/data/ticker.php"; 
+	private final String MT_GOX_URL = "https://mtgox.com/code/data/ticker.php";
+	private Calendar lastDisplayed = null;
 		
         @Override
         public String getCommandName() {
@@ -22,19 +25,33 @@ public class jIBCBitcoin extends jIBCommand {
         @Override
         protected void handleMessage(jIRCBot bot, String channel, String sender,
                         String message) {
+        	if(message.toLowerCase().equals(getCommandName())) {
+        		sendBitcoinUpdate(bot, channel);
+        	} else if(message.contains(getCommandName())) {
+        		if(lastDisplayed != null) {
+        			long now = Calendar.getInstance().getTimeInMillis();
+        			if(now - lastDisplayed.getTimeInMillis() > 600000) { // 10 Minutes
+        				sendBitcoinUpdate(bot, channel);
+        			}
+        		} else {
+        			sendBitcoinUpdate(bot, channel);
+        		}
+        	}
+	   }
+        
+        private void sendBitcoinUpdate(jIRCBot bot, String channel) {
         	Object content = null;
         	Gson gson = new Gson();
-        	if(message.toLowerCase().equals(getCommandName())) {
-        		content = jIRCTools.getUrlContent(MT_GOX_URL);
-        		if(content != null) {
+            content = jIRCTools.getUrlContent(MT_GOX_URL);
+        	if(content != null) {
         			String json = content.toString();
         			// Massage json to fit our class a bit
         			json = json.substring(10, json.lastIndexOf('}'));
         			MtGoxTicker ticker = gson.fromJson(json, MtGoxTicker.class);
         			bot.sendMessage(channel, "Mt. Gox: Last=" + ticker.last + " High=" + ticker.high + " Low=" + ticker.low);
-        		} else {
+        			lastDisplayed = Calendar.getInstance();
+        	} else {
         			bot.sendMessage(channel, "Mt. Gox appears to be down right now");
-        		}
-        	}
-	}
+        	}   	
+        }
 }
