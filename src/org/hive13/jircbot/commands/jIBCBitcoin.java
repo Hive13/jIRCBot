381411111716478a91cbb4e94734da7bbc952e66
Paul Vincent <pvince@gmail.com>
2011-06-14 11:20:55 -0400

    Two things: - Removed the LAZOR RssReader - Updated jIBCBitcoin to give more descriptive errors.

--- src/org/hive13/jircbot/commands/jIBCBitcoin.java
+++ src/org/hive13/jircbot/commands/jIBCBitcoin.java
@@ -1,6 +1,11 @@
 package org.hive13.jircbot.commands;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.SocketTimeoutException;
 import java.util.Calendar;
+
+import javax.net.ssl.SSLHandshakeException;
 
 import org.hive13.jircbot.jIRCBot;
 import org.hive13.jircbot.support.MtGoxTicker;
@@ -9,9 +14,9 @@ import org.hive13.jircbot.support.jIRCTools;
 import com.google.gson.Gson;
 
 public class jIBCBitcoin extends jIBCommand {
-	private final String MT_GOX_URL = "https://mtgox.com/code/data/ticker.php";
-	private Calendar lastDisplayed = null;
-		
+   private final String MT_GOX_URL = "https://mtgox.com/code/data/ticker.php";
+   private Calendar lastDisplayed = null;
+      
         @Override
         public String getCommandName() {
                 return "bitcoin";
@@ -25,33 +30,49 @@ public class jIBCBitcoin extends jIBCommand {
         @Override
         protected void handleMessage(jIRCBot bot, String channel, String sender,
                         String message) {
-        	if(message.toLowerCase().equals(getCommandName())) {
-        		sendBitcoinUpdate(bot, channel);
-        	} else if(message.contains(getCommandName())) {
-        		if(lastDisplayed != null) {
-        			long now = Calendar.getInstance().getTimeInMillis();
-        			if(now - lastDisplayed.getTimeInMillis() > 600000) { // 10 Minutes
-        				sendBitcoinUpdate(bot, channel);
-        			}
-        		} else {
-        			sendBitcoinUpdate(bot, channel);
-        		}
-        	}
-	   }
+           if(message.toLowerCase().equals(getCommandName())) {
+              sendBitcoinUpdate(bot, channel);
+           } else if(message.contains(getCommandName())) {
+              if(lastDisplayed != null) {
+                 long now = Calendar.getInstance().getTimeInMillis();
+                 if(now - lastDisplayed.getTimeInMillis() > 600000) { // 10 Minutes
+                    sendBitcoinUpdate(bot, channel);
+                 }
+              } else {
+                 sendBitcoinUpdate(bot, channel);
+              }
+           }
+      }
         
         private void sendBitcoinUpdate(jIRCBot bot, String channel) {
-        	Object content = null;
-        	Gson gson = new Gson();
+           Object content = null;
+           Gson gson = new Gson();
+           String errorMsg = "";
+           try {
             content = jIRCTools.getUrlContent(MT_GOX_URL);
-        	if(content != null) {
-        			String json = content.toString();
-        			// Massage json to fit our class a bit
-        			json = json.substring(10, json.lastIndexOf('}'));
-        			MtGoxTicker ticker = gson.fromJson(json, MtGoxTicker.class);
-        			bot.sendMessage(channel, "Mt. Gox: Last=" + ticker.last + " High=" + ticker.high + " Low=" + ticker.low);
-        			lastDisplayed = Calendar.getInstance();
-        	} else {
-        			bot.sendMessage(channel, "Mt. Gox appears to be down right now");
-        	}   	
+         } catch (MalformedURLException e) {
+            errorMsg = "Mt. Gox is reporting an invalid URL.";
+            e.printStackTrace();
+         } catch (SocketTimeoutException e) {
+            errorMsg = "Mt. Gox appears to be down right now";
+            e.printStackTrace();
+         } catch (SSLHandshakeException e) {
+            errorMsg = "Mt. Gox gave us an SSL Handshake error.";
+            e.printStackTrace();
+         } catch (IOException e) {
+            errorMsg = "Mt. Gox had an IOException? Seriously?";
+            e.printStackTrace();
+         }
+
+           if(content != null) {
+                 String json = content.toString();
+                 // Massage json to fit our class a bit
+                 json = json.substring(10, json.lastIndexOf('}'));
+                 MtGoxTicker ticker = gson.fromJson(json, MtGoxTicker.class);
+                 bot.sendMessage(channel, "Mt. Gox: Last=" + ticker.last + " High=" + ticker.high + " Low=" + ticker.low);
+                 lastDisplayed = Calendar.getInstance();
+           } else {
+                 bot.sendMessage(channel, errorMsg);
+           }      
         }
 }
