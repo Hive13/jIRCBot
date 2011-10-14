e61ccb71d54b26ae8f2e12cbe7dab56ecf5b2925
ryodoan <pvince@gmail.com>
2011-10-13 22:15:20 -0400

    Fixes issue #17 1. Added a new CommandThread that runs every 2 minutes and performs Maintenance tasks.  One of the tasks of this is to check for connectedness. 2. If a CommandThread fails to send a msg due to disconnect, it initiates a reconnect. 3. There is now a common "connectBot()" function that sets a boolean to "abConnecting" to true or false depending on if the bot is currently connecting.

---
+++ src/org/hive13/jircbot/commands/jIBCTMaintThread.java
@@ -0,0 +1,52 @@
+package org.hive13.jircbot.commands;
+import org.hive13.jircbot.jIRCBot;
+import org.hive13.jircbot.jIRCBot.eLogLevel;
+import org.hive13.jircbot.support.jIRCUser.eAuthLevels;
+
+
+/**
+ * This classes main purpose is to perform regular maintenance tasks that
+ * might crop in while the bot is running.
+ * 
+ * Examples of these tasks:
+ * - Check if we are still connected
+ * - Check the 'op' status of the users.
+ * 
+ * @author vincentp
+ *
+ */
+public class jIBCTMaintThread extends jIBCommandThread {
+
+	private static long dfLoopDelay = 120000; // 120 seconds, 2 minutes.
+	
+	public jIBCTMaintThread(jIRCBot bot, String commandName, String channel) {
+		this(bot, commandName, channel, dfLoopDelay, eAuthLevels.admin);
+	}
+
+	public jIBCTMaintThread(jIRCBot bot, String commandName, String channel,
+			long loopDelay, eAuthLevels authLevel) {
+		super(bot, commandName, channel, loopDelay, authLevel);
+	}
+
+	@Override
+	public void loop() {
+		// Check if the bot is currently connected and is not currently connecting.
+		if(!bot.isConnected() && !bot.abConnecting.get()) {
+			bot.log(this.commandName + " - Bot is not connected.  Initiating Reconnected.", eLogLevel.error);
+			bot.connectBot();
+			if(bot.isConnected()) {
+				bot.log(this.commandName + " - Bot is reconnected! Huzzah!", eLogLevel.info);
+			} else {
+				bot.log(this.commandName + " - Bot failed to reconnect... see you in " + Long.toString((dfLoopDelay / 1000)) + " seconds", eLogLevel.severe);
+			}
+		}
+	}
+
+	@Override
+	public String getHelp() {
+		String cmdName = getSimpleCommandName();
+		return "The following are valid uses of this command: !" + cmdName + " help ;" +
+				" !" + cmdName + " start ; !" + cmdName + " stop";
+	}
+
+}
