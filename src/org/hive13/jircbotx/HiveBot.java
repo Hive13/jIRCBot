866cd257db6f9571144fb2aa034d2cd97fa918da
Paul <pvince@gmail.com>
2013-07-23 09:52:40 -0400

    Updated the twitter search parameters to exclude another irrelevant tag hijacker.

--- src/org/hive13/jircbotx/HiveBot.java
+++ src/org/hive13/jircbotx/HiveBot.java
@@ -76,7 +76,7 @@ public class HiveBot {
       bot.getListenerManager().addListener(new Quit());
       bot.getListenerManager().addListener(new Plugins(bot));
       bot.getListenerManager().addListener(new UserAuth());
-      bot.getListenerManager().addListener(new TwitterSearch(bot, "Twitter", botChannel, "hive13 -b_hive13 -katerinabonvora -thehive_berlin -danielleabroad -joelix -jennifuchs"));
+      bot.getListenerManager().addListener(new TwitterSearch(bot, "Twitter", botChannel, "hive13 -2versa -b_hive13 -katerinabonvora -thehive_berlin -danielleabroad -joelix -jennifuchs"));
       
       try {
          bot.getListenerManager().addListener(new RssReader(bot, "DoorAlert", botChannel, "http://www.hive13.org/isOpen/RSS.php"));
